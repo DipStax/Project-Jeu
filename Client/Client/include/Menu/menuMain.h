2f039a05db81605708ced3ac1944225003b58550
Hugo PREVOST <DipStax.hugo@gmail.com>
2019-08-21 23:24:37 +0200

    Revert "Initial commit"
    This reverts commit 989e25ccae2fa6d555092c57cb5b78d0dac2b7e2.

--- Client/Client/include/Menu/menuMain.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <Bouton/BoutonKeyEmail.h>
-
-namespace mMain {
-	void setMenu(std::vector<std::unique_ptr<Bouton>>& listBtn, short adrs);
-	int verifEvent(sf::Event event, short& adrs, std::vector<std::unique_ptr<Bouton>>& list_btn, std::vector<sf::Text>& listTxt, sf::RenderWindow& screen);
-	void affErr(std::string error, std::vector<sf::Text>& listTxt, sf::Font& font);
-}
