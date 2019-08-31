5003a45b98e971ec33c4dea6ed6e850c42242af3
Hugo PREVOST <DipStax.hugo@gmail.com>
2019-08-31 23:04:06 +0200

    Delete some fonction

--- Serveur_User/Serveur_User/include/Gestion/PersoJson.h
+++ Serveur_User/Serveur_User/include/Gestion/PersoJson.h
@@ -11,15 +11,14 @@ namespace gjs {
 	void genListPerso(std::vector<std::unique_ptr<Perso>>& listPerso, int ID, std::vector<ObjectFile> listObjFile);
 	void genPerso(std::unique_ptr<Perso>& perso, nlohmann::json& pjson);
 
-	stuff genStuff(nlohmann::json& pjson, int i, std::vector<ObjectFile> listObjFile);
 	stuff genStuff(nlohmann::json& ijson, std::vector<ObjectFile> listObjFile);
 	enchant genEnchant(nlohmann::json& pjson, int i, ObjectFile statEnchante);
 	enchant genEnchant(nlohmann::json& ijson, ObjectFile statEnchante);
 	item genItem(nlohmann::json& ijson);
-	void genItem(nlohmann::json& ijson, std::unique_ptr<sac>& sac_, std::vector<ObjectFile> listObjFile);
+	void genItemSac(nlohmann::json& ijson, std::unique_ptr<sac>& sac_, std::vector<ObjectFile> listObjFile);
 
-	int pvMax(TYPEPERSO type);
-	int manaMax(TYPEPERSO type);
+	int pvMax(TYPEPERSO type, int lvl);
+	int manaMax(TYPEPERSO type, int lvl);
 	statistic genStat(int ID, ObjectFile stat);
 
 	void genFileStat(std::vector<ObjectFile> listObjFile);
